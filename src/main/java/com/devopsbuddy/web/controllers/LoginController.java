67c618b33b04a9981bb6511bdb1f3d2f56e22eff
larris <fantidisl@gmail.com>
2016-09-01 10:24:50 +0300

    Added login form

---
+++ src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by larris on 01/09/16.
+ */
+
+@Controller
+public class LoginController {
+
+    public  static  final  String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public  String login (){
+        return  LOGIN_VIEW_NAME;
+    }
+
+}
