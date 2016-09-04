019725113fbdfe22c505903f43cf1cd6c7ae193b
larris <fantidisl@gmail.com>
2016-09-05 00:30:50 +0300

    Created the JPA Entity model for spring security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,67 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by larris on 05/09/16.
+ */
+@Entity
+public class Role implements Serializable {
+
+    /** The Serial Verision UID for serializable Classes */
+    private static final long serialVersionUID = 1L;
+
+    public Role() {
+    }
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
