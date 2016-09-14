f47aa2de3468eba57a6b7c3ed05aef2fc4effc1e
larris <fantidisl@gmail.com>
2016-09-14 13:08:22 +0300

    Completed the forgotmypassowrd implementation

--- src/main/java/com/devopsbuddy/web/controllers/ForgotMyPasswordController.java
+++ src/main/java/com/devopsbuddy/web/controllers/ForgotMyPasswordController.java
@@ -3,21 +3,29 @@ package com.devopsbuddy.web.controllers;
 import com.devopsbuddy.backend.persistence.domain.backend.PasswordResetToken;
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.backend.service.EmailService;
-import com.devopsbuddy.backend.service.PasswordResetTokenService;
-import com.devopsbuddy.utils.UserUtils;
 import com.devopsbuddy.backend.service.I18NService;
+import com.devopsbuddy.backend.service.PasswordResetTokenService;
+import com.devopsbuddy.backend.service.UserService;
+import com.devopsbuddy.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Locale;
 
 /**
  * Created by larris on 11/09/16.
@@ -38,11 +46,20 @@ public class ForgotMyPasswordController {
 
     public static final String EMAIL_MESSAGE_PROPERTY_NAME = "forgotmypassword.mail.text";
 
+    public static final String CHANGE_PASSWORD_VIEW_NAME = "/forgotmypassword/changePassword";
+
+    private static final String PASSWORD_RESET_ATTRIBUTE_NAME = "passwordReset";
+
+    private static final String MESSAGE_ATTRIBUTE_NAME = "message";
+
     @Autowired
     private I18NService i18NService;
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private UserService userService;
 
     @Value("${webmaster.email}")
     private String webMasterEmail;
@@ -84,6 +101,80 @@ public class ForgotMyPasswordController {
         model.addAttribute(MAIL_SENT_KEY,"true");
 
         return EMAIL_ADDRESS_VIEW_NAME;
+    }
+
+    @RequestMapping(value = CHANGE_PASSWORD_PATH, method = RequestMethod.GET)
+    public String changeUserPasswordGet(@RequestParam("id") long id,
+                                        @RequestParam("token") String token,
+                                        Locale locale,
+                                        ModelMap model){
+        if (StringUtils.isEmpty(token) || id == 0){
+            LOG.error("Invalid userid {} or token {}",id,token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,"Invalid User Id or Token value");
+            return  CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        PasswordResetToken passwordResetToken = passwordResetTokenService.findByToken(token);
+
+        if (null == passwordResetToken){
+            LOG.warn("A token could not be found with the value {}",token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,"Token not found");
+            return  CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        User user = passwordResetToken.getUser();
+
+        if (user.getId() != id){
+            LOG.error("The user id {} passed as a parameter does not match the use id {} associated with the token {} ",id,user.getId(),token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,i18NService.getMessage("resetPassword.token.invalid",locale));
+            return  CHANGE_PASSWORD_VIEW_NAME;
+
+        }
+
+        if (LocalDateTime.now(Clock.systemUTC()).isAfter(passwordResetToken.getExpiryDate())){
+            LOG.error("The token {} has expired",token);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,i18NService.getMessage("resetPassword.token.expired",locale));
+            return  CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        model.addAttribute("principalId",user.getId());
+        Authentication auth = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        return  CHANGE_PASSWORD_VIEW_NAME;
+    }
+
+    @RequestMapping(value = CHANGE_PASSWORD_PATH, method = RequestMethod.POST)
+    public String changeUserPasswordPost(@RequestParam("prinicpal_id") long userId,
+                                         @RequestParam("password") String password,
+                                         ModelMap model){
+
+        Authentication authentication =SecurityContextHolder.getContext().getAuthentication();
+        if (null == authentication){
+            LOG.error("An unauthenticated user tried to invoke the POST method");
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,"You are not authorized to perform this request");
+            return  CHANGE_PASSWORD_VIEW_NAME;
+        }
+
+        User user = (User) authentication.getPrincipal();
+        if(user.getId() == userId){
+            LOG.error("Security breach !! User {} is trying to make a passowrd reset on behalf of {}",user.getId(),userId);
+            model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"false");
+            model.addAttribute(MESSAGE_ATTRIBUTE_NAME,"You are not authorized to perform this request");
+            return  CHANGE_PASSWORD_VIEW_NAME;
+
+        }
+
+        userService.updateUserPassword(userId,password);
+        LOG.info("Password succesfully update for user {}",user.getUsername());
+        model.addAttribute(PASSWORD_RESET_ATTRIBUTE_NAME,"true");
+
+        return  CHANGE_PASSWORD_VIEW_NAME;
     }
 
 }
